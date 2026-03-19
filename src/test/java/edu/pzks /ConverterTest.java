6639ec253d514b79804eddcbf8da1024269d2366
Ilona Chupa <ilonachupa@gmail.com>
2026-03-19 10:57:14 +0200

    Clean up comments in ConverterTest.java
    Removed author and project metadata comments from ConverterTest.

--- src/test/java/edu/pzks /ConverterTest.java
+++ src/test/java/edu/pzks /ConverterTest.java
@@ -7,14 +7,6 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-/*
-  @author   george
-  @project   lab_3_4_test
-  @class  ConverterTest
-  @version  1.0.0 
-  @since 10.03.25 - 14.05
-*/
-
 public class ConverterTest {
 
     @BeforeEach
