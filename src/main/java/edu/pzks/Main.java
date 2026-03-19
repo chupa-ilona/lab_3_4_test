837af113de3ab58fa789f898824a5d3f8d23b888
Ilona Chupa <ilonachupa@gmail.com>
2026-03-19 10:52:52 +0200

    Update and rename RomanToInteger.java to Main.java
    LAB 4.
    Roman to Integer.Exception Handling

---
+++ src/main/java/edu/pzks/Main.java
@@ -0,0 +1,9 @@
+package edu.pzks;
+
+public class Main {
+    public static final String ROMAN_NUMBER = "MCMXCV";
+    public static void main(String[] args) {
+        System.out.println(Converter.romanToArabic(ROMAN_NUMBER));
+
+    }
+}
